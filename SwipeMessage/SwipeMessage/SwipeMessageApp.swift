eccbcdae01c0ca991cbb17bb9ebf9ede54f435bd
jwon0523 <jwon010523@gmail.com>
2024-03-19 02:27:26 +0900

    Implemente swipe gesture using DrageGesture

---
+++ SwipeMessage/SwipeMessage/SwipeMessageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeMessageApp.swift
+//  SwipeMessage
+//
+//  Created by jaewon Lee on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SwipeMessageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
