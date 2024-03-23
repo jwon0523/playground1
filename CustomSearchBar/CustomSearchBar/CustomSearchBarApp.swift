fc9f3d0b5407b5bfec3fbd507b8486f5d6082912
jwon0523 <jwon010523@gmail.com>
2024-03-23 18:20:23 +0900

    Implemente CustomSearchBar

---
+++ CustomSearchBar/CustomSearchBar/CustomSearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSearchBarApp.swift
+//  CustomSearchBar
+//
+//  Created by jaewon Lee on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomSearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
